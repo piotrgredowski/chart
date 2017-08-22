fdc698b83e4e77d2aa9f57efdd30a1ddea4b1afc
Piotr Gredowski <piotrgredowski@gmail.com>
2017-08-23 01:04:20 +0200

    Many modifications
    Some tries with chart. Trying to find good way to import c3, d3 & moment to tsc.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var moment = require("../js/moment.min");
+// import * as d3 from '../js/d3.min';
+// import * as c3 from '../js/c3.min';
 function generateData(name, probes, divider) {
     if (divider === void 0) { divider = 5; }
     var x = [];
@@ -5,18 +10,23 @@ function generateData(name, probes, divider) {
     x[0] = 'x';
     y[0] = name;
     for (var i = 1; i <= probes; i++) {
-        y[i] = Math.sin(i / divider);
-        x[i] = i;
+        y[i] = divider + Math.sin(i / divider) + Math.floor(Math.random() * divider);
+        // x[i] = 2000 + i;
+        // x[i] = x[i].toString();
+        // x[i] = moment().add(i, 'd').format();
+        x[i] = moment().add(i, 'd');
     }
     var result = [x, y];
     return result;
 }
-console.log(5);
 var chart = c3.generate({
     bindto: '#chart',
     size: {
-        width: 640,
+        width: 800,
         height: 480
+    },
+    padding: {
+        right: 20
     },
     data: {
         x: 'x',
@@ -24,15 +34,17 @@ var chart = c3.generate({
     },
     axis: {
         x: {
-            show: true,
-            type: 'indexed',
+            type: 'timeseries',
             tick: {
-                // culling: {
-                // 	max: 10
-                // },
-                // fit: true,
-                // count: 5,
-                rotate: 45
+                fit: false,
+                format: '%Y/%m/%d',
+                rotate: 30
+            }
+        },
+        y: {
+            show: true,
+            tick: {
+                format: d3.format("$.2f")
             }
         }
     },
@@ -40,15 +52,24 @@ var chart = c3.generate({
         enabled: true
     },
     point: {
-        show: true,
+        show: false,
         focus: {
             expand: {
                 enabled: true,
                 r: 3
             }
         }
+    },
+    subchart: {
+        show: false,
+        size: {
+            height: 150
+        }
     }
 });
 chart.load({
     columns: generateData('sin2', 100, 10)
 });
+setTimeout(function () {
+    chart.transform('spline', 'sin2');
+}, 3000);
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,46 +1,58 @@
+import * as moment from '../js/moment.min';
+// import * as d3 from '../js/d3.min';
+// import * as c3 from '../js/c3.min';
 function generateData(name: string, probes: number, divider = 5) {
 	let x : (string|number)[] = [];
 	let y : (string|number)[] = [];
 	x[0] = 'x';
 	y[0] = name;
 	for (let i = 1; i <= probes; i++) {
-		y[i] = Math.sin( i / divider );
-		x[i] = i;
+		y[i] = divider + Math.sin(i/divider)+Math.floor(Math.random()*divider);
+		// x[i] = 2000 + i;
+		// x[i] = x[i].toString();
+		// x[i] = moment().add(i, 'd').format();
+		x[i] = moment().add(i, 'd');
 	}
 	let result = [x, y];
 	return result;
 }
-console.log(5);
-let chart: object = c3.generate({
+let chart = c3.generate({
 	bindto: '#chart',
 	size: {
-		width: 640,
+		width: 800,
 		height: 480
+	},
+	padding: {
+
+		right: 20
 	},
 	data: {
 		x: 'x',
-		columns: generateData('sin1', 100)
+		columns: generateData('sin1', 100),
 	},
 	axis: {
 		x: {
-			show: true,
-			type: 'indexed',
+			type: 'timeseries',
 			tick: {
-				// culling: {
-				// 	max: 10
-				// },
-				// fit: true,
-				// count: 5,
-				rotate: 45,
+				fit: false,
+				format: '%Y/%m/%d',
+				rotate: 30,
+			}
+		},
+		y: {
+			show: true,
+			tick: {
+				format: d3.format("$.2f")
 			}
 		}
 	},
 	zoom: {
 		enabled: true,
+		// onzoom: () => console.log("zoom"),
 		// rescale: true
 	},
 	point: {
-		show: true,
+		show: false,
 		focus: {
 			expand: {
 				enabled: true,
@@ -48,8 +60,18 @@ let chart: object = c3.generate({
 			}
 		}
 	},
+	subchart: {
+		show: false,
+		size: {
+			height: 150
+		}
+	}
 });
 
 chart.load({
 	columns: generateData('sin2', 100, 10)
 });
+
+setTimeout(function () {
+	chart.transform('spline', 'sin2');
+}, 3000);
